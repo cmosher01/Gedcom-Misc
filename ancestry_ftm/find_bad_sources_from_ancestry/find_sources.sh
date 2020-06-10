3f47d92dff595cf72cf7306828d175c9e1f90a0e
Christopher A. Mosher <cmosher01@gmail.com>
2020-06-10 16:30:25 -0400

    fix handling of paths for called shell scripts

--- ancestry_ftm/find_bad_sources_from_ancestry/find_sources.sh
+++ ancestry_ftm/find_bad_sources_from_ancestry/find_sources.sh
@@ -2,9 +2,13 @@ if [ ! "$1" ] ; then
     echo "usage: $0 input.ged" >&2
     exit 1
 fi
+
+me="$(readlink -f "$0")"
+here="$(dirname "$me")"
+
 grep -E ' _APID |^0 .* INDI|^0 .* FAM|1 NAME|1 HUSB|1 WIFE|1 CHIL' "$1" | \
 cut -d' ' -f2- | \
-python find_sources_with_mulitple_people.py | \
+python $here/find_sources_with_mulitple_people.py | \
 sort -u | \
-python find_sources_with_mulitple_people_part_2.py | \
+python $here/find_sources_with_mulitple_people_part_2.py | \
 sort -u
