b152c6ae6b218bcc0195d52c1966bb774dcdd711
Christopher A. Mosher <cmosher01@gmail.com>
2022-04-04 21:34:53 -0400

    build secure pdf

--- tools/build_publishable_pdf.sh
+++ tools/build_publishable_pdf.sh
@@ -5,13 +5,13 @@
 # convert (ImageMagick)
 # qpdf
 
-if [ -z "$3" ] ; then
-    echo "usage: $0 lyx-or-odt-file user-pass owner-pass"
-    exit 1
-fi
+# if [ -z "$3" ] ; then
+#     echo "usage: $0 lyx-or-odt-file user-pass owner-pass"
+#     exit 1
+# fi
 
 cvt_opts="-alpha flatten -density 300 -compress jpeg -quality 35 -sampling-factor 4:2:0 -type TrueColor -interlace plane -define jpeg:dct-method=float -strip"
-encrypt_opts="--verbose --replace-input --encrypt $2 $3 256 --extract=n --assemble=n --annotate=n --form=n --modify-other=n --print=none --"
+encrypt_opts="--verbose --replace-input --linearize   --encrypt $2 $3 256 --extract=n --assemble=n --annotate=n --form=n --modify-other=n --print=none --cleartext-metadata --"
 show_opts="--password=$2 --check --show-object=trailer --show-pages --with-images"
 
 if [ ! -e "$1" ] ; then
@@ -19,13 +19,14 @@ if [ ! -e "$1" ] ; then
     exit 1
 fi
 
-nam="${1%.*}"
-ext="${1##*.}"
+base="$(basename $1)"
+nam="${base%.*}"
+ext="${base##*.}"
 
 if [ $ext = lyx ] ; then
     # convert lyx to pdf
     lyx -batch -E pdf4 $nam-txt.pdf $nam.lyx
-elif [ $ext = context ] ; then
+elif [ $ext = context -o $ext = tex ] ; then
     context --debug --result=$nam-txt.pdf $1
 elif [ $ext = fodt -o $ext = odt ] ; then
     # export original ODT source document to (text-based) PDF file
@@ -36,18 +37,53 @@ else
     exit 1
 fi
 
+
+
+mkdir -p $nam
+cd $nam
+rm *.jpg
+convert $cvt_opts ../$nam-txt.pdf $nam.jpg
+exiftool -tagsfromfile ../../$nam.xmp -xmp:all -overwrite_original_in_place *.jpg
+cd -
+
+cp -v ../$nam.xmp ./
+
+SecurePdf $nam \
+    --keystore=/srv/arc/mosher.mine.nu.p12 \
+    --graphic=../../sigstamp.png \
+    --page=2
+
+sha384sum -b $nam.pdf >$nam.pdf.sha384
+cat $nam.pdf.sha384
+hash_value=$(cat $nam.pdf.sha384 | cut -d\  -f1 | xxd -r -p - | base32 -w 0 -)
+echo "urn:hash:application/pdf:sha384:$hash_value"
+
+
+
+
+
+
 # build PDF with JPEG pages
-convert $cvt_opts $nam-txt.pdf $nam-jpg.pdf
+# convert $cvt_opts $nam-txt.pdf $nam-jpg.pdf
 
 # encrypt PDF files
-qpdf $encrypt_opts $nam-txt.pdf
-qpdf $encrypt_opts $nam-jpg.pdf
+# qpdf $encrypt_opts $nam-txt.pdf
+# qpdf $encrypt_opts $nam-jpg.pdf
+
+# copy metadata to jpg file
+# exiftool -password "$2" -TagsFromFile $nam-txt.pdf '-xmp:all<all' $nam-jpg.pdf
 
 # show PDF stats
-qpdf $show_opts $nam-txt.pdf
-qpdf $show_opts $nam-jpg.pdf
+# qpdf $show_opts $nam-txt.pdf
+# pdfinfo -upw "$2" $nam-txt.pdf
+# exiftool -password "$2" -a -G1 -s $nam-txt.pdf
 
-ls -lhtr --full-time $nam*
+# qpdf $show_opts $nam-jpg.pdf
+# pdfinfo -upw "$2" $nam-jpg.pdf
+# exiftool -password "$2" -a -G1 -s $nam-jpg.pdf
+# exiftool -password "$2" -xmp -b $nam-jpg.pdf >$nam.xmp.xml
+
+# ls -lht --full-time
 
 
 
@@ -95,4 +131,5 @@ ls -lhtr --full-time $nam*
 
 
 
-# NOTE: use i7j-rups to view internal structure of PDF files
+# NOTE: use i7j-rups to view internal structure of PDF files:
+# java -jar /usr/local/lib/java/itext-rups.jar
